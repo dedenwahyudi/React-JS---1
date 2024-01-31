568f344a901b888bfb0217927b68ee7e411016fc
dedenwahyudi <id.dedenwahyudi@gmail.com>
2024-01-31 14:54:28 +0700

    Button Component with Class Component and Functional Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,52 @@
+import React from "react";
+
+class Button extends React.Component {
+  render() {
+    return (
+      <button
+        className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white"
+        type="submit"
+      >
+        Buy now
+      </button>
+    );
+  }
+}
+
+function ButtonBlack() {
+  return (
+    <button
+      className="h-10 px-6 font-semibold rounded-md bg-black text-white"
+      type="submit"
+    >
+      Buy now
+    </button>
+  );
+}
+
+const ButtonRed = () => {
+  return (
+    <button
+      className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white"
+      type="submit"
+    >
+      Buy now
+    </button>
+  );
+};
+
 function App() {
   return (
     <div className="flex justify-center bg-blue-600 min-h-screen items-center">
-      <div>Hello, World!</div>
+      <div className="flex gap-x-3">
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <Button />
+        <ButtonBlack />
+        <ButtonRed />
+      </div>
     </div>
   );
 }
