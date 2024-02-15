538c2aea1194333c76e8d27fda5e2a5e380eb05c
dedenwahyudi <id.dedenwahyudi@gmail.com>
2024-02-15 16:42:57 +0700

    Nested Component - Versi 2

--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -3,48 +3,42 @@ import Button from "../Elements/Button";
 const CardProduct = (props) => {
   const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border-gray-700 rounded-lg shadow">
+    <div className="w-full max-w-sm bg-gray-800 border-gray-700 rounded-lg shadow mx-2">
       {children}
     </div>
   );
 };
 
-const Body = () => {
+const Header = (props) => {
+  const { image } = props;
+  return (
+    <a href="">
+      <img src={image} alt="products" className="p-8 rounded-t-lg" />
+    </a>
+  );
+};
+
+const Body = (props) => {
+  const { children, title } = props;
   return (
     <div className="px-5 pb-5">
       <a href="">
         <h5 className="text-s font-semibold tracking-tight text-white">
-          Sepatu baru
+          {title}
         </h5>
-        <p className="text-m text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eveniet
-          soluta numquam sapiente voluptatum labore, suscipit repellat. Quasi
-          hic, labore quidem, esse mollitia laborum est, asperiores facilis
-          repellat explicabo omnis?
-        </p>
+        <p className="text-m text-white">{children}</p>
       </a>
     </div>
   );
 };
 
-const Footer = () => {
+const Footer = (props) => {
+  const { price } = props;
   return (
     <div className="flex items-center justify-between px-5 pb-5">
-      <span className="text-xl font-bold text-white">Rp 1.000.000</span>
+      <span className="text-xl font-bold text-white">{price}</span>
       <Button classname="bg-blue-600">Add To Cart</Button>
     </div>
-  );
-};
-
-const Header = () => {
-  return (
-    <a href="">
-      <img
-        src="/public/images/shoes-1.jpg"
-        alt="products"
-        className="p-8 rounded-t-lg"
-      />
-    </a>
   );
 };
 
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -4,9 +4,24 @@ const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
       <CardProduct>
-        <CardProduct.Header></CardProduct.Header>
-        <CardProduct.Body></CardProduct.Body>
-        <CardProduct.Footer></CardProduct.Footer>
+        <CardProduct.Header image="/public/images/shoes-1.jpg" />
+        <CardProduct.Body title="Sepatu Baru">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eveniet
+          soluta numquam sapiente voluptatum labore, suscipit repellat. Quasi
+          hic, labore quidem, esse mollitia laborum est, asperiores facilis
+          repellat explicabo omnis?
+        </CardProduct.Body>
+        <CardProduct.Footer price="Rp 1.000.000" />
+      </CardProduct>
+      <CardProduct>
+        <CardProduct.Header image="/public/images/shoes-1.jpg" />
+        <CardProduct.Body title="Sepatu Baru">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eveniet
+          soluta numquam sapiente voluptatum labore, suscipit repellat. Quasi
+          hic, labore quidem, esse mollitia laborum est, asperiores facilis
+          repellat explicabo omnis?
+        </CardProduct.Body>
+        <CardProduct.Footer price="Rp 1.000.000" />
       </CardProduct>
     </div>
   );
