ec3a5af32ecde16ff346bdf5550b6bcc90ada65d
dedenwahyudi <id.dedenwahyudi@gmail.com>
2024-02-15 14:26:22 +0700

    Conditional Rendering - Ternary Operator dan &&

--- src/components/Layouts/AuthLayouts.jsx
+++ src/components/Layouts/AuthLayouts.jsx
@@ -1,5 +1,7 @@
+import { Link } from "react-router-dom";
+
 const AuthLayout = (props) => {
-  const { children, title } = props;
+  const { children, title, type } = props;
   return (
     <div className="flex justify-center min-h-screen items-center">
       <div className="w-full max-w-xs">
@@ -8,6 +10,23 @@ const AuthLayout = (props) => {
           Welcome, Please enter your details
         </p>
         {children}
+
+        <p className="text-sm mt-5 text-center">
+          {type === "login"
+            ? "Dont't have an account? "
+            : "Already have an account? "}
+
+          {type === "login" && (
+            <Link to="/register" className="font-bold text-blue-600">
+              Register
+            </Link>
+          )}
+          {type === "register" && (
+            <Link to="/login" className="font-bold text-blue-600">
+              Login
+            </Link>
+          )}
+        </p>
       </div>
     </div>
   );
