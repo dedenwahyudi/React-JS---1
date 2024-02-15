55148909e4c19e372e9aff2b6da2b4f1fb3db805
dedenwahyudi <id.dedenwahyudi@gmail.com>
2024-02-15 22:30:26 +0700

    Event Handler

--- src/components/Fragments/FormLogin.jsx
+++ src/components/Fragments/FormLogin.jsx
@@ -2,8 +2,17 @@ import Button from "../Elements/Button";
 import InputForm from "../Elements/Input";
 
 const FormLogin = () => {
+  const handleLogin = (event) => {
+    event.preventDefault();
+
+    localStorage.setItem("email", event.target.email.value);
+    localStorage.setItem("password", event.target.password.value);
+
+    window.location.href = "/products";
+  };
+
   return (
-    <form action="">
+    <form onSubmit={handleLogin}>
       <InputForm
         label="Email"
         name="email"
@@ -16,7 +25,9 @@ const FormLogin = () => {
         type="password"
         placeholder="********"
       />
-      <Button classname="bg-blue-600">Login</Button>
+      <Button classname="bg-blue-600" type="submit">
+        Login
+      </Button>
     </form>
   );
 };
--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -1,4 +1,6 @@
+import { Fragment } from "react";
 import CardProduct from "../components/Fragments/CardProduct";
+import Button from "../components/Elements/Button";
 
 const products = [
   {
@@ -7,9 +9,9 @@ const products = [
     price: "Rp 1.000.000",
     image: "/public/images/shoes-1.jpg",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eveniet
-    soluta numquam sapiente voluptatum labore, suscipit repellat. Quasi
-    hic, labore quidem, esse mollitia laborum est, asperiores facilis
-    repellat explicabo omnis?`,
+            soluta numquam sapiente voluptatum labore, suscipit repellat. Quasi
+            hic, labore quidem, esse mollitia laborum est, asperiores facilis
+            repellat explicabo omnis?`,
   },
   {
     id: 2,
@@ -17,7 +19,7 @@ const products = [
     price: "Rp 500.000",
     image: "/public/images/shoes-1.jpg",
     description: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis eveniet
-    soluta numquam sapiente voluptatum labore.`,
+            soluta numquam sapiente voluptatum labore.`,
   },
   {
     id: 3,
@@ -28,19 +30,35 @@ const products = [
   },
 ];
 
+const email = localStorage.getItem("email");
+
 const ProductsPage = () => {
+  const handleLogout = () => {
+    localStorage.removeItem("email");
+    localStorage.removeItem("password");
+    window.location.href = "/login";
+  };
+
   return (
-    <div className="flex justify-center py-5">
-      {products.map((product) => (
-        <CardProduct>
-          <CardProduct.Header image={product.image} />
-          <CardProduct.Body name={product.name}>
-            {product.description}
-          </CardProduct.Body>
-          <CardProduct.Footer price={product.price} />
-        </CardProduct>
-      ))}
-    </div>
+    <Fragment>
+      <div className="flex justify-end h-20 bg-blue-600 text-white items-center px-10">
+        {email}
+        <Button classname="ml-5 bg-black" onClick={handleLogout}>
+          Logout
+        </Button>
+      </div>
+      <div className="flex justify-center py-5">
+        {products.map((product) => (
+          <CardProduct key={product.id}>
+            <CardProduct.Header image={product.image} />
+            <CardProduct.Body name={product.name}>
+              {product.description}
+            </CardProduct.Body>
+            <CardProduct.Footer price={product.price} />
+          </CardProduct>
+        ))}
+      </div>
+    </Fragment>
   );
 };
 
