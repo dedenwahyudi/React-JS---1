cd306fe8f3412ed55f6c4ca5c264245dc336efd7
dedenwahyudi <id.dedenwahyudi@gmail.com>
2024-02-16 16:38:01 +0700

    useState - 2

--- src/pages/products.jsx
+++ src/pages/products.jsx
@@ -47,13 +47,15 @@ const ProductsPage = () => {
   };
 
   const handleAddToCart = (id) => {
-    setCart([
-      ...cart,
-      {
-        id,
-        qty: 1,
-      },
-    ]);
+    if (cart.find((item) => item.id === id)) {
+      setCart(
+        cart.map((item) =>
+          item.id === id ? { ...item, qty: item.qty + 1 } : item
+        )
+      );
+    } else {
+      setCart([...cart, { id, qty: 1 }]);
+    }
   };
 
   return (
