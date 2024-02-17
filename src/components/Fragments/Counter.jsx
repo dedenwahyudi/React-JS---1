357d71257f2ea7473938e309f2ebc8633fd45272
dedenwahyudi <id.dedenwahyudi@gmail.com>
2024-02-17 15:30:54 +0700

    Lifecycle

--- src/components/Fragments/Counter.jsx
+++ src/components/Fragments/Counter.jsx
@@ -6,6 +6,19 @@ class Counter extends React.Component {
     this.state = {
       count: 0,
     };
+    console.log("constructor");
+  }
+
+  componentDidMount() {
+    this.setState({ count: 1 });
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+    if (this.state.count === 10) {
+      this.setState({ count: 5 });
+    }
   }
 
   render() {
@@ -18,6 +31,7 @@ class Counter extends React.Component {
         >
           +
         </button>
+        {console.log("render")}
       </div>
     );
   }
