ef2db9a1e9a8aaf7257ea02816bd0d02efd048dc
dedenwahyudi <id.dedenwahyudi@gmail.com>
2024-08-12 13:08:37 +0700

    useContext

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -1,12 +1,15 @@
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useLogin } from "../../hooks/useLogin";
 import Button from "../Elements/Button";
 import { useSelector } from "react-redux";
+import { DarkMode } from "../../context/DarkMode";
 
 const Navbar = () => {
   const username = useLogin();
   const [totalCart, setTotalCart] = useState(0);
   const cart = useSelector((state) => state.cart.data);
+
+  const { isDarkMode, setIsDarkMode } = useContext(DarkMode);
 
   useEffect(() => {
     const sum = cart.reduce((acc, item) => {
@@ -27,9 +30,15 @@ const Navbar = () => {
       <Button classname="ml-5 bg-black" onClick={handleLogout}>
         Logout
       </Button>
-      <div className="flex items-center bg-gray-800 p-2 rounded-md ml-5">
+      <div className="flex items-center bg-gray-800 p-2 rounded-md ml-5 mr-5">
         {totalCart}
       </div>
+      <Button
+        className="bg-black right-2 top-2 p-2 px-10 m-5 text-white rounded"
+        onClick={() => setIsDarkMode(!isDarkMode)}
+      >
+        {isDarkMode ? "Light" : "Dark"}
+      </Button>
     </div>
   );
 };
